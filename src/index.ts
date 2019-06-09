c5e385f8484dc640544f11b4867683ed9692805c
tomlai19852004 <tomlaihosum@hotmail.com>
2019-06-09 12:07:36 +0800

    1.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-export { default as FacebookMessenger } from './FacebookMessenger';
-export { default as facebookMiddleware } from './FacebookMiddleware';
-export { default as facebookSenderInfoMiddleware } from './FacebookSenderInfoMiddleware';
-export { default as facebookSenderActionMiddleware } from './FacebookSenderActionMiddleware';
+export { default as FacebookMessenger } from "./FacebookMessenger";
+export { default as facebookMiddleware } from "./FacebookMiddleware";
+export {
+  default as facebookSenderInfoMiddleware
+} from "./FacebookSenderInfoMiddleware";
+export {
+  default as facebookSenderActionMiddleware
+} from "./FacebookSenderActionMiddleware";
