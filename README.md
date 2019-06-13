48404af0d2a87600cebc9a20075a566a159747b1
tomlai19852004 <tomlaihosum@hotmail.com>
2019-06-13 18:19:11 +0800

    added README.md

---
+++ README.md
@@ -0,0 +1,20 @@
+### Getting Started
+
+Create a record in MongoDB Config collection for the following record
+
+`
+{
+    "uni": "Unique Virtual Agent Identification",
+    "key": "Value of CONFIG_FACEBOOK_ACCESS_TOKEN",
+    "value": "Value of Facebook Access Token"
+}
+
+{
+    "uni": "Unique Virtual Agent Identification",
+    "key": "Value of CONFIG_FACEBOOK_VERIFY_TOKEN",
+    "value": "Value of Facebook Verify Token"
+}
+`
+
+
+
--- src/Types.ts
+++ src/Types.ts
@@ -1,266 +1,266 @@
 enum FileType {
-  AUDIO = "audio",
-  FALLBACK = "fallback",
-  FILE = "file",
-  IMAGE = "image",
-  LOCATION = "location",
-  VIDEO = "video"
+	AUDIO = "audio",
+	FALLBACK = "fallback",
+	FILE = "file",
+	IMAGE = "image",
+	LOCATION = "location",
+	VIDEO = "video"
 }
 
 enum ResponseMessageType {
-  AUDIO = "audio",
-  FILE = "file",
-  IMAGE = "image",
-  VIDEO = "video",
-  TEMPLATE = "template"
+	AUDIO = "audio",
+	FILE = "file",
+	IMAGE = "image",
+	VIDEO = "video",
+	TEMPLATE = "template"
 }
 
 enum ResponseMessageTemplateType {
-  BUTTON = "button",
-  GENERIC = "generic"
+	BUTTON = "button",
+	GENERIC = "generic"
 }
 
 enum ButtonType {
-  WEB_URL = "web_url",
-  POSTBACK = "postback"
+	WEB_URL = "web_url",
+	POSTBACK = "postback"
 }
 
 enum WebUrlButtonWebviewHeightRatio {
-  COMPACT = "compact",
-  TALL = "tall",
-  FULL = "full"
+	COMPACT = "compact",
+	TALL = "tall",
+	FULL = "full"
 }
 
 enum QuickReplyMessageContentType {
-  TEXT = "text",
-  LOCATION = "location"
+	TEXT = "text",
+	LOCATION = "location"
 }
 
 // https://developers.facebook.com/docs/messenger-platform/send-messages#messaging_types
 enum MessagingType {
-  RESPONSE = "RESPONSE",
-  UPDATE = "UPDATE",
-  MESSAGE_TAG = "MESSAGE_TAG",
-  NON_PROMOTIONAL_SUBSCRIPTION = "NON_PROMOTIONAL_SUBSCRIPTION"
+	RESPONSE = "RESPONSE",
+	UPDATE = "UPDATE",
+	MESSAGE_TAG = "MESSAGE_TAG",
+	NON_PROMOTIONAL_SUBSCRIPTION = "NON_PROMOTIONAL_SUBSCRIPTION"
 }
 
 // Request
 
 interface BaseRequestMessaging {
-  sender: {
-    id: string;
-  };
-  recipient: {
-    id: string;
-  };
-  timestamp: number;
+	sender: {
+		id: string;
+	};
+	recipient: {
+		id: string;
+	};
+	timestamp: number;
 }
 
 interface TextRequestMessaging extends BaseRequestMessaging {
-  message: {
-    mid: string;
-    text: string;
-    quick_reply?: {
-      payload: string;
-    };
-  };
+	message: {
+		mid: string;
+		text: string;
+		quick_reply?: {
+			payload: string;
+		};
+	};
 }
 
 interface FileRequestMessaging extends BaseRequestMessaging {
-  message: {
-    mid: string;
-    attachments: Array<{
-      type: FileType.AUDIO | FileType.FILE | FileType.IMAGE | FileType.VIDEO;
-      payload: {
-        url: string;
-      };
-    }>;
-  };
+	message: {
+		mid: string;
+		attachments: Array<{
+			type: FileType.AUDIO | FileType.FILE | FileType.IMAGE | FileType.VIDEO;
+			payload: {
+				url: string;
+			};
+		}>;
+	};
 }
 
 interface LocationRequestMessaging extends BaseRequestMessaging {
-  message: {
-    mid: string;
-    attachments: Array<{
-      type: FileType.LOCATION;
-      payload: {
-        coordinates: {
-          lat: number;
-          long: number;
-        };
-      };
-    }>;
-  };
+	message: {
+		mid: string;
+		attachments: Array<{
+			type: FileType.LOCATION;
+			payload: {
+				coordinates: {
+					lat: number;
+					long: number;
+				};
+			};
+		}>;
+	};
 }
 
 interface FallbackRequestMessaging extends BaseRequestMessaging {
-  message: {
-    mid: string;
-    text: string;
-    attachments: Array<{
-      type: FileType.FALLBACK;
-      title: string;
-      url: string;
-    }>;
-  };
+	message: {
+		mid: string;
+		text: string;
+		attachments: Array<{
+			type: FileType.FALLBACK;
+			title: string;
+			url: string;
+		}>;
+	};
 }
 
 interface PostBackMessaging {
-  postback: {
-    title: string;
-    payload: string;
-    referral?: {
-      ref: string;
-      source: string;
-      type: string;
-    };
-  };
+	postback: {
+		title: string;
+		payload: string;
+		referral?: {
+			ref: string;
+			source: string;
+			type: string;
+		};
+	};
 }
 
 type RequestMessaging =
-  | TextRequestMessaging
-  | FileRequestMessaging
-  | LocationRequestMessaging
-  | FallbackRequestMessaging
-  | PostBackMessaging;
+	| TextRequestMessaging
+	| FileRequestMessaging
+	| LocationRequestMessaging
+	| FallbackRequestMessaging
+	| PostBackMessaging;
 
 interface Request {
-  object: string;
-  entry: Array<{
-    id: string;
-    time: number;
-    messaging: RequestMessaging[];
-  }>;
+	object: string;
+	entry: Array<{
+		id: string;
+		time: number;
+		messaging: RequestMessaging[];
+	}>;
 }
 
 // Response
 
 interface TextResponseMessage {
-  text: string;
+	text: string;
 }
 
 interface FileResponseMessage {
-  attachment: {
-    type:
-      | ResponseMessageType.AUDIO
-      | ResponseMessageType.FILE
-      | ResponseMessageType.IMAGE
-      | ResponseMessageType.VIDEO;
-    payload: {
-      url: string;
-    };
-  };
+	attachment: {
+		type:
+			| ResponseMessageType.AUDIO
+			| ResponseMessageType.FILE
+			| ResponseMessageType.IMAGE
+			| ResponseMessageType.VIDEO;
+		payload: {
+			url: string;
+		};
+	};
 }
 
 interface PostBackButton {
-  type: ButtonType.POSTBACK;
-  title: string;
-  payload: string;
+	type: ButtonType.POSTBACK;
+	title: string;
+	payload: string;
 }
 
 interface WebUrlButton {
-  type: ButtonType.WEB_URL;
-  url: string;
-  title: string;
-  webview_height_ratio?: WebUrlButtonWebviewHeightRatio;
-  messenger_extensions?: boolean;
-  fallback_url?: string;
+	type: ButtonType.WEB_URL;
+	url: string;
+	title: string;
+	webview_height_ratio?: WebUrlButtonWebviewHeightRatio;
+	messenger_extensions?: boolean;
+	fallback_url?: string;
 }
 
 type Button = WebUrlButton | PostBackButton;
 
 interface ButtonTemplate {
-  template_type: ResponseMessageTemplateType.BUTTON;
-  text: string;
-  buttons: Button[];
+	template_type: ResponseMessageTemplateType.BUTTON;
+	text: string;
+	buttons: Button[];
 }
 
 interface GenericTemplate {
-  template_type: ResponseMessageTemplateType.GENERIC;
-  elements: Array<{
-    title: string;
-    subtitle?: string;
-    image_url?: string;
-    default_action?: Button;
-    buttons?: Button[];
-  }>;
+	template_type: ResponseMessageTemplateType.GENERIC;
+	elements: Array<{
+		title: string;
+		subtitle?: string;
+		image_url?: string;
+		default_action?: Button;
+		buttons?: Button[];
+	}>;
 }
 
 interface TemplateResponseMessage {
-  attachment: {
-    type: ResponseMessageType.TEMPLATE;
-    payload: ButtonTemplate | GenericTemplate;
-  };
+	attachment: {
+		type: ResponseMessageType.TEMPLATE;
+		payload: ButtonTemplate | GenericTemplate;
+	};
 }
 
 interface BaseQuickReply {
-  content_type: QuickReplyMessageContentType;
-  title?: string;
-  payload?: string;
-  image_url?: string;
+	content_type: QuickReplyMessageContentType;
+	title?: string;
+	payload?: string;
+	image_url?: string;
 }
 
 interface TextQuickReply extends BaseQuickReply {
-  content_type: QuickReplyMessageContentType.TEXT;
-  title: string;
-  payload: string;
+	content_type: QuickReplyMessageContentType.TEXT;
+	title: string;
+	payload: string;
 }
 
 interface LocationQuickReply extends BaseQuickReply {
-  content_type: QuickReplyMessageContentType.LOCATION;
+	content_type: QuickReplyMessageContentType.LOCATION;
 }
 
 type QuickReply = TextQuickReply | LocationQuickReply;
 
 interface QuickReplyMessage {
-  text: string;
-  quick_replies: QuickReply[];
+	text: string;
+	quick_replies: QuickReply[];
 }
 
 type ResponseMessage =
-  | TextResponseMessage
-  | FileResponseMessage
-  | TemplateResponseMessage
-  | QuickReplyMessage;
+	| TextResponseMessage
+	| FileResponseMessage
+	| TemplateResponseMessage
+	| QuickReplyMessage;
 
 interface Response {
-  recipient: {
-    id: string;
-  };
-  message: ResponseMessage;
-  messaging_type: MessagingType;
-  tag?: string;
+	recipient: {
+		id: string;
+	};
+	message: ResponseMessage;
+	messaging_type: MessagingType;
+	tag?: string;
 }
 
 export {
-  FileType,
-  ResponseMessageType,
-  ResponseMessageTemplateType,
-  ButtonType,
-  WebUrlButtonWebviewHeightRatio,
-  QuickReplyMessageContentType,
-  MessagingType,
-  BaseRequestMessaging,
-  TextRequestMessaging,
-  FileRequestMessaging,
-  LocationRequestMessaging,
-  FallbackRequestMessaging,
-  PostBackMessaging,
-  RequestMessaging,
-  Request,
-  TextResponseMessage,
-  FileResponseMessage,
-  PostBackButton,
-  WebUrlButton,
-  Button,
-  ButtonTemplate,
-  GenericTemplate,
-  TemplateResponseMessage,
-  BaseQuickReply,
-  TextQuickReply,
-  LocationQuickReply,
-  QuickReply,
-  QuickReplyMessage,
-  ResponseMessage,
-  Response
+	FileType,
+	ResponseMessageType,
+	ResponseMessageTemplateType,
+	ButtonType,
+	WebUrlButtonWebviewHeightRatio,
+	QuickReplyMessageContentType,
+	MessagingType,
+	BaseRequestMessaging,
+	TextRequestMessaging,
+	FileRequestMessaging,
+	LocationRequestMessaging,
+	FallbackRequestMessaging,
+	PostBackMessaging,
+	RequestMessaging,
+	Request,
+	TextResponseMessage,
+	FileResponseMessage,
+	PostBackButton,
+	WebUrlButton,
+	Button,
+	ButtonTemplate,
+	GenericTemplate,
+	TemplateResponseMessage,
+	BaseQuickReply,
+	TextQuickReply,
+	LocationQuickReply,
+	QuickReply,
+	QuickReplyMessage,
+	ResponseMessage,
+	Response
 };
