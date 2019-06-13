4884617585c2dbadb6dbb8718ae4898123174c39
tomlai19852004 <tomlaihosum@hotmail.com>
2019-06-13 23:30:30 +0800

    added typing reply

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Create a record in MongoDB Config collection for the following record
 
-`
+```
 {
     "uni": "Unique Virtual Agent Identification",
     "key": "Value of CONFIG_FACEBOOK_ACCESS_TOKEN",
@@ -14,7 +14,7 @@ Create a record in MongoDB Config collection for the following record
     "key": "Value of CONFIG_FACEBOOK_VERIFY_TOKEN",
     "value": "Value of Facebook Verify Token"
 }
-`
+```
 
 
 
--- src/FacebookSenderInfoMiddleware.ts
+++ src/FacebookSenderInfoMiddleware.ts
@@ -2,51 +2,51 @@ import * as req from "request-promise-native";
 import { isNil } from "lodash";
 import { Middleware } from "lexica-dialog-core/dist/Api";
 import {
-  MESSENGER_NAME,
-  CONFIG_FACEBOOK_API_BASE_URL,
-  CONFIG_FACEBOOK_ACCESS_TOKEN
+	MESSENGER_NAME,
+	CONFIG_FACEBOOK_API_BASE_URL,
+	CONFIG_FACEBOOK_ACCESS_TOKEN
 } from "./Constants";
 
 const facebookSenderInfoMiddleware: Middleware = async (context, next) => {
-  const {
-    uni,
-    uniConfigs,
-    messenger,
-    request,
-    senderInfo,
-    senderInfoRepository
-  } = context;
-  if (
-    messenger.name === MESSENGER_NAME &&
-    !isNil(request) &&
-    isNil(senderInfo)
-  ) {
-    const { senderId } = request;
-    const apiBaseUrl = uniConfigs.get(CONFIG_FACEBOOK_API_BASE_URL)
-      .value as string;
-    const accessToken = uniConfigs.get(CONFIG_FACEBOOK_ACCESS_TOKEN)
-      .value as string;
-    const response = await req({
-      json: true,
-      qs: {
-        access_token: uniConfigs.get(CONFIG_FACEBOOK_ACCESS_TOKEN)
-          .value as string,
-        fields: "first_name,last_name,middle_name"
-      },
-      url: `${apiBaseUrl}/${senderId}`
-    });
-    context.senderInfo = await senderInfoRepository.create({
-      creationDate: new Date(),
-      firstName: response.first_name,
-      lastName: response.last_name,
-      lastUpdatedDate: new Date(),
-      messenger: MESSENGER_NAME,
-      middleName: response.middle_name,
-      senderId,
-      uni
-    });
-  }
-  await next();
+	const {
+		uni,
+		uniConfigs,
+		messenger,
+		request,
+		senderInfo,
+		senderInfoRepository
+	} = context;
+	if (
+		messenger.name === MESSENGER_NAME &&
+		!isNil(request) &&
+		isNil(senderInfo)
+	) {
+		const { senderId } = request;
+		const apiBaseUrl = uniConfigs.get(CONFIG_FACEBOOK_API_BASE_URL)
+			.value as string;
+		const accessToken = uniConfigs.get(CONFIG_FACEBOOK_ACCESS_TOKEN)
+			.value as string;
+		const response = await req({
+			json: true,
+			qs: {
+				access_token: uniConfigs.get(CONFIG_FACEBOOK_ACCESS_TOKEN)
+					.value as string,
+				fields: "first_name,last_name,middle_name"
+			},
+			url: `${apiBaseUrl}/${senderId}`
+		});
+		context.senderInfo = await senderInfoRepository.create({
+			creationDate: new Date(),
+			firstName: response.first_name,
+			lastName: response.last_name,
+			lastUpdatedDate: new Date(),
+			messenger: MESSENGER_NAME,
+			middleName: response.middle_name,
+			senderId,
+			uni
+		});
+	}
+	await next();
 };
 
 export default facebookSenderInfoMiddleware;
