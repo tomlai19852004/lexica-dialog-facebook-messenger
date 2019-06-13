aab4e318466231f120b5a6d3e12cdffdfb74b5f4
tomlai19852004 <tomlaihosum@hotmail.com>
2019-06-13 23:30:41 +0800

    1.0.5

--- src/FacebookMiddleware.ts
+++ src/FacebookMiddleware.ts
@@ -2,38 +2,38 @@ import { Middleware } from "lexica-dialog-core/dist/Api";
 import { MESSENGER_NAME, CONFIG_FACEBOOK_VERIFY_TOKEN } from "./Constants";
 
 const facebookMiddleware: Middleware = async (context, next) => {
-	const { serverContext, messenger, uniConfigs, logger } = context;
-	if (messenger.name === MESSENGER_NAME) {
-		let verifyToken;
+  const { serverContext, messenger, uniConfigs, logger } = context;
+  if (messenger.name === MESSENGER_NAME) {
+    let verifyToken;
 
-		if (uniConfigs.has(CONFIG_FACEBOOK_VERIFY_TOKEN)) {
-			verifyToken = uniConfigs.get(CONFIG_FACEBOOK_VERIFY_TOKEN)
-				.value as string;
-		} else {
-			throw new Error("UNI Config not found: " + CONFIG_FACEBOOK_VERIFY_TOKEN);
-		}
+    if (uniConfigs.has(CONFIG_FACEBOOK_VERIFY_TOKEN)) {
+      verifyToken = uniConfigs.get(CONFIG_FACEBOOK_VERIFY_TOKEN)
+        .value as string;
+    } else {
+      throw new Error("UNI Config not found: " + CONFIG_FACEBOOK_VERIFY_TOKEN);
+    }
 
-		if (
-			serverContext.method === "GET" &&
-			serverContext.query["hub.mode"] === "subscribe" &&
-			serverContext.query["hub.verify_token"] === verifyToken
-		) {
-			serverContext.status = 200;
-			serverContext.body = serverContext.query["hub.challenge"];
-		} else if (serverContext.method === "POST") {
-			// Async
-			const { rawRequest } = context;
-			serverContext.status = 200;
-			serverContext.body = "OK";
-			next().catch(error =>
-				logger.error("Error catched by Facebook middleware", rawRequest, error)
-			);
-		} else {
-			await next();
-		}
-	} else {
-		await next();
-	}
+    if (
+      serverContext.method === "GET" &&
+      serverContext.query["hub.mode"] === "subscribe" &&
+      serverContext.query["hub.verify_token"] === verifyToken
+    ) {
+      serverContext.status = 200;
+      serverContext.body = serverContext.query["hub.challenge"];
+    } else if (serverContext.method === "POST") {
+      // Async
+      const { rawRequest } = context;
+      serverContext.status = 200;
+      serverContext.body = "OK";
+      next().catch(error =>
+        logger.error("Error catched by Facebook middleware", rawRequest, error)
+      );
+    } else {
+      await next();
+    }
+  } else {
+    await next();
+  }
 };
 
 export default facebookMiddleware;
--- src/FacebookSenderActionMiddleware.ts
+++ src/FacebookSenderActionMiddleware.ts
@@ -2,54 +2,56 @@ import * as req from "request-promise-native";
 import { isNil } from "lodash";
 import { Middleware, RunTimeConfig } from "lexica-dialog-core/dist/Api";
 import {
-	MESSENGER_NAME,
-	CONFIG_FACEBOOK_API_BASE_URL,
-	CONFIG_FACEBOOK_ACCESS_TOKEN
+  MESSENGER_NAME,
+  CONFIG_FACEBOOK_API_BASE_URL,
+  CONFIG_FACEBOOK_ACCESS_TOKEN
 } from "./Constants";
 
 function sendAction(
-	senderId: string,
-	apiBaseUrl: string,
-	accessToken: string,
-	senderAction: string
+  senderId: string,
+  apiBaseUrl: string,
+  accessToken: string,
+  senderAction: string
 ) {
-	return req({
-		json: {
-			recipient: {
-				id: senderId
-			},
-			sender_action: senderAction
-		},
-		method: "POST",
-		qs: {
-			access_token: accessToken
-		},
-		uri: `${apiBaseUrl}/me/messages`
-	});
+  return req({
+    json: {
+      recipient: {
+        id: senderId
+      },
+      sender_action: senderAction
+    },
+    method: "POST",
+    qs: {
+      access_token: accessToken
+    },
+    uri: `${apiBaseUrl}/me/messages`
+  });
 }
 
 const facebookSenderActionMiddleware: Middleware = async (context, next) => {
-	const { uniConfigs, messenger, request, logger } = context;
-	if (messenger.name === MESSENGER_NAME && !isNil(request)) {
-		const { senderId } = request;
-		const apiBaseUrl = uniConfigs.get(CONFIG_FACEBOOK_API_BASE_URL)
-			.value as string;
-		const accessToken = uniConfigs.get(CONFIG_FACEBOOK_ACCESS_TOKEN)
-			.value as string;
+  const { uniConfigs, messenger, request, logger } = context;
+  if (messenger.name === MESSENGER_NAME && !isNil(request)) {
+    const { senderId } = request;
+    const apiBaseUrl = uniConfigs.get(CONFIG_FACEBOOK_API_BASE_URL)
+      .value as string;
+    const accessToken = uniConfigs.get(CONFIG_FACEBOOK_ACCESS_TOKEN)
+      .value as string;
 
-		const actionItems : any = [
-			sendAction(senderId, apiBaseUrl, accessToken, "mark_seen"),
-		];
+    const actionItems: any = [
+      sendAction(senderId, apiBaseUrl, accessToken, "mark_seen")
+    ];
 
-		if( uniConfigs.get(RunTimeConfig.SUSPEND_AUTO_REPLY) ){
-			actionItems.push( sendAction(senderId, apiBaseUrl, accessToken, "typing_on") );
-		}
+    if (uniConfigs.get(RunTimeConfig.SUSPEND_AUTO_REPLY)) {
+      actionItems.push(
+        sendAction(senderId, apiBaseUrl, accessToken, "typing_on")
+      );
+    }
 
-		Promise.all(actionItems).catch(error =>
-			logger.error("Call Facebook sender action with error", error)
-		);
-	}
-	await next();
+    Promise.all(actionItems).catch(error =>
+      logger.error("Call Facebook sender action with error", error)
+    );
+  }
+  await next();
 };
 
 export default facebookSenderActionMiddleware;
